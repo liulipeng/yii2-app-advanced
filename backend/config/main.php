c9364fb1103b0f507fa90e0325b69453f59d2982
刘李鹏 <liu.lipeng@icloud.com>
2016-07-05 11:47:02 +0800

    测试构建

--- backend/config/main.php
+++ backend/config/main.php
@@ -92,6 +92,7 @@ return [
         'allowActions' => [
             'debug/*',
             'site/*',
+            'admin/*',
 //            'admin/*',
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
